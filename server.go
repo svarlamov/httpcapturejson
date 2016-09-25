5c67e05a5792841684f21fb2e2665ab0a6c2dec1
Sasha Varlamov <sasha@sashavarlamov.com>
2016-09-25 13:22:37 +0800

    Timestamps

--- server.go
+++ server.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 type JSON map[string]interface{}
 
-
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Got an HTTP", r.Method, "Request on", r.URL.Path)
-	fmt.Println("Content-Type:", r.Header.Get("Content-Type"))
+	fmt.Println(time.Now().String(), ":", "Got an HTTP", r.Method, "Request on", r.URL.Path)
+	fmt.Println(time.Now().String(), ":", "Content-Type:", r.Header.Get("Content-Type"))
 	decoder := json.NewDecoder(r.Body)
 	var reqObj JSON
 	err := decoder.Decode(&reqObj)
 	if err != nil {
-		fmt.Println("JSON Error:", err)
+		fmt.Println(time.Now().String(), ":", "JSON Error:", err)
 		return
 	}
-	fmt.Println("JSON Body:", reqObj)
+	fmt.Println(time.Now().String(), ":", "JSON Body:", reqObj)
 	fmt.Fprintf(w, "{\"status\":\"ok\"")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Starting HTTP Server")
+	fmt.Println(time.Now().String(), ":", "Starting HTTP Server")
 	http.ListenAndServe(":3000", nil)
 }
